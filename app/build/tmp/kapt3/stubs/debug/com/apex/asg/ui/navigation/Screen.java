fdf8144ab6bd12bcd1664714bbd71873be72ce57
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-19 01:12:45 +0530

    Update Screen.java

--- app/build/tmp/kapt3/stubs/debug/com/apex/asg/ui/navigation/Screen.java
+++ app/build/tmp/kapt3/stubs/debug/com/apex/asg/ui/navigation/Screen.java
@@ -1,6 +1,6 @@
 package com.apex.asg.ui.navigation;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000D\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0010\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\b7\u0018\u00002\u00020\u0001:\n\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016B%\b\u0004\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\b\b\u0002\u0010\u0004\u001a\u00020\u0003\u0012\n\b\u0002\u0010\u0005\u001a\u0004\u0018\u00010\u0006\u00a2\u0006\u0002\u0010\u0007R\u0013\u0010\u0005\u001a\u0004\u0018\u00010\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\b\u0010\tR\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\n\u0010\u000bR\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\u000b\u0082\u0001\n\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f \u00a8\u0006!"}, d2 = {"Lcom/apex/asg/ui/navigation/Screen;", "", "route", "", "title", "icon", "Landroidx/compose/ui/graphics/vector/ImageVector;", "(Ljava/lang/String;Ljava/lang/String;Landroidx/compose/ui/graphics/vector/ImageVector;)V", "getIcon", "()Landroidx/compose/ui/graphics/vector/ImageVector;", "getRoute", "()Ljava/lang/String;", "getTitle", "AIAgent", "DistrictMap", "Events", "HackathonOrganizer", "Home", "NAACRecords", "Onboarding", "Profile", "Repository", "Splash", "Lcom/apex/asg/ui/navigation/Screen$AIAgent;", "Lcom/apex/asg/ui/navigation/Screen$DistrictMap;", "Lcom/apex/asg/ui/navigation/Screen$Events;", "Lcom/apex/asg/ui/navigation/Screen$HackathonOrganizer;", "Lcom/apex/asg/ui/navigation/Screen$Home;", "Lcom/apex/asg/ui/navigation/Screen$NAACRecords;", "Lcom/apex/asg/ui/navigation/Screen$Onboarding;", "Lcom/apex/asg/ui/navigation/Screen$Profile;", "Lcom/apex/asg/ui/navigation/Screen$Repository;", "Lcom/apex/asg/ui/navigation/Screen$Splash;", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000T\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\b7\u0018\u00002\u00020\u0001:\u000e\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001aB%\b\u0004\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\b\b\u0002\u0010\u0004\u001a\u00020\u0003\u0012\n\b\u0002\u0010\u0005\u001a\u0004\u0018\u00010\u0006\u00a2\u0006\u0002\u0010\u0007R\u0013\u0010\u0005\u001a\u0004\u0018\u00010\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\b\u0010\tR\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\n\u0010\u000bR\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\u000b\u0082\u0001\u000e\u001b\u001c\u001d\u001e\u001f !\"#$%&\'(\u00a8\u0006)"}, d2 = {"Lcom/apex/asg/ui/navigation/Screen;", "", "route", "", "title", "icon", "Landroidx/compose/ui/graphics/vector/ImageVector;", "(Ljava/lang/String;Ljava/lang/String;Landroidx/compose/ui/graphics/vector/ImageVector;)V", "getIcon", "()Landroidx/compose/ui/graphics/vector/ImageVector;", "getRoute", "()Ljava/lang/String;", "getTitle", "AIAgent", "Chat", "DistrictMap", "Events", "HackathonOrganizer", "Home", "Jobs", "Login", "NAACRecords", "Onboarding", "Profile", "Register", "Repository", "Splash", "Lcom/apex/asg/ui/navigation/Screen$AIAgent;", "Lcom/apex/asg/ui/navigation/Screen$Chat;", "Lcom/apex/asg/ui/navigation/Screen$DistrictMap;", "Lcom/apex/asg/ui/navigation/Screen$Events;", "Lcom/apex/asg/ui/navigation/Screen$HackathonOrganizer;", "Lcom/apex/asg/ui/navigation/Screen$Home;", "Lcom/apex/asg/ui/navigation/Screen$Jobs;", "Lcom/apex/asg/ui/navigation/Screen$Login;", "Lcom/apex/asg/ui/navigation/Screen$NAACRecords;", "Lcom/apex/asg/ui/navigation/Screen$Onboarding;", "Lcom/apex/asg/ui/navigation/Screen$Profile;", "Lcom/apex/asg/ui/navigation/Screen$Register;", "Lcom/apex/asg/ui/navigation/Screen$Repository;", "Lcom/apex/asg/ui/navigation/Screen$Splash;", "app_debug"})
 public abstract class Screen {
     @org.jetbrains.annotations.NotNull()
     private final java.lang.String route = null;
@@ -34,6 +34,15 @@ public abstract class Screen {
         public static final com.apex.asg.ui.navigation.Screen.AIAgent INSTANCE = null;
         
         private AIAgent() {
+        }
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/apex/asg/ui/navigation/Screen$Chat;", "Lcom/apex/asg/ui/navigation/Screen;", "()V", "app_debug"})
+    public static final class Chat extends com.apex.asg.ui.navigation.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.apex.asg.ui.navigation.Screen.Chat INSTANCE = null;
+        
+        private Chat() {
         }
     }
     
@@ -73,6 +82,24 @@ public abstract class Screen {
         }
     }
     
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/apex/asg/ui/navigation/Screen$Jobs;", "Lcom/apex/asg/ui/navigation/Screen;", "()V", "app_debug"})
+    public static final class Jobs extends com.apex.asg.ui.navigation.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.apex.asg.ui.navigation.Screen.Jobs INSTANCE = null;
+        
+        private Jobs() {
+        }
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/apex/asg/ui/navigation/Screen$Login;", "Lcom/apex/asg/ui/navigation/Screen;", "()V", "app_debug"})
+    public static final class Login extends com.apex.asg.ui.navigation.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.apex.asg.ui.navigation.Screen.Login INSTANCE = null;
+        
+        private Login() {
+        }
+    }
+    
     @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/apex/asg/ui/navigation/Screen$NAACRecords;", "Lcom/apex/asg/ui/navigation/Screen;", "()V", "app_debug"})
     public static final class NAACRecords extends com.apex.asg.ui.navigation.Screen {
         @org.jetbrains.annotations.NotNull()
@@ -97,6 +124,15 @@ public abstract class Screen {
         public static final com.apex.asg.ui.navigation.Screen.Profile INSTANCE = null;
         
         private Profile() {
+        }
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/apex/asg/ui/navigation/Screen$Register;", "Lcom/apex/asg/ui/navigation/Screen;", "()V", "app_debug"})
+    public static final class Register extends com.apex.asg.ui.navigation.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.apex.asg.ui.navigation.Screen.Register INSTANCE = null;
+        
+        private Register() {
         }
     }
     
