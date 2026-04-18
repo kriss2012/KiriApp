9fb86d583d11c45ddc41c52d3f3eed0d44e18fab
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-19 01:13:44 +0530

    Update ProfileScreenKt.java

--- app/build/tmp/kapt3/stubs/debug/com/apex/asg/ui/screens/ProfileScreenKt.java
+++ app/build/tmp/kapt3/stubs/debug/com/apex/asg/ui/screens/ProfileScreenKt.java
@@ -1,14 +1,21 @@
 package com.apex.asg.ui.screens;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000 \n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\u001a\b\u0010\u0000\u001a\u00020\u0001H\u0007\u001a\b\u0010\u0002\u001a\u00020\u0001H\u0007\u001a\u0010\u0010\u0003\u001a\u00020\u00012\u0006\u0010\u0004\u001a\u00020\u0005H\u0007\u001a\b\u0010\u0006\u001a\u00020\u0001H\u0007\u001a\b\u0010\u0007\u001a\u00020\u0001H\u0007\u001a\u001c\u0010\b\u001a\u00020\u0001*\u00020\t2\u0006\u0010\n\u001a\u00020\u000b2\u0006\u0010\f\u001a\u00020\u000bH\u0007\u00a8\u0006\r"}, d2 = {"AchievementChipsRow", "", "DashboardMenu", "DashboardMenuCard", "item", "Lcom/apex/asg/ui/screens/DashboardMenuItem;", "ProfileHeroSection", "ProfileScreen", "StatBox", "Landroidx/compose/foundation/layout/RowScope;", "value", "", "label", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u00006\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\u001a\b\u0010\u0000\u001a\u00020\u0001H\u0007\u001a\b\u0010\u0002\u001a\u00020\u0001H\u0007\u001a\u0010\u0010\u0003\u001a\u00020\u00012\u0006\u0010\u0004\u001a\u00020\u0005H\u0007\u001a\u0010\u0010\u0006\u001a\u00020\u00012\u0006\u0010\u0007\u001a\u00020\bH\u0007\u001a\u001a\u0010\t\u001a\u00020\u00012\u0006\u0010\u0007\u001a\u00020\b2\b\b\u0002\u0010\n\u001a\u00020\u000bH\u0007\u001a\u0012\u0010\f\u001a\u00020\u00012\b\b\u0002\u0010\r\u001a\u00020\u000eH\u0007\u001a\u001c\u0010\u000f\u001a\u00020\u0001*\u00020\u00102\u0006\u0010\u0011\u001a\u00020\u00122\u0006\u0010\u0013\u001a\u00020\u0012H\u0007\u00a8\u0006\u0014"}, d2 = {"AchievementChipsRow", "", "DashboardMenu", "DashboardMenuCard", "item", "Lcom/apex/asg/ui/screens/DashboardMenuItem;", "ProfileContent", "user", "Lcom/apex/asg/data/remote/UserDto;", "ProfileHeroSection", "isVerified", "", "ProfileScreen", "viewModel", "Lcom/apex/asg/ui/viewmodels/ProfileViewModel;", "StatBox", "Landroidx/compose/foundation/layout/RowScope;", "value", "", "label", "app_debug"})
 public final class ProfileScreenKt {
     
     @androidx.compose.runtime.Composable()
-    public static final void ProfileScreen() {
+    public static final void ProfileScreen(@org.jetbrains.annotations.NotNull()
+    com.apex.asg.ui.viewmodels.ProfileViewModel viewModel) {
     }
     
     @androidx.compose.runtime.Composable()
-    public static final void ProfileHeroSection() {
+    public static final void ProfileContent(@org.jetbrains.annotations.NotNull()
+    com.apex.asg.data.remote.UserDto user) {
+    }
+    
+    @androidx.compose.runtime.Composable()
+    public static final void ProfileHeroSection(@org.jetbrains.annotations.NotNull()
+    com.apex.asg.data.remote.UserDto user, boolean isVerified) {
     }
     
     @androidx.compose.runtime.Composable()
