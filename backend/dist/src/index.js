00790fdd5bb8a07d2a61611f0bf8930cac549693
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-19 11:11:26 +0530

    Update index.js

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -24,7 +24,8 @@ const io = new Server(httpServer, {
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
