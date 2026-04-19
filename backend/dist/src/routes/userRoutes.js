a052961153875fca008074217aa7b3999d872485
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-19 21:12:10 +0530

    Update userRoutes.js

--- backend/dist/src/routes/userRoutes.js
+++ backend/dist/src/routes/userRoutes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { getProfile, updateProfile, getAllVerifiedUsers, toggleEventAccess, searchUsers } from '../controllers/userController.js';
+import { getAllVerifiedUsers, getProfile, searchUsers, toggleEventAccess, updateProfile, getActivities, getUserStats, getCollegeActivity, verifyActivity } from '../controllers/userController.js';
 const router = Router();
 router.get('/', searchUsers);
 router.get('/profile/:userId', getProfile);
 router.put('/profile/:userId', updateProfile);
 router.put('/toggle-access/:userId', toggleEventAccess);
 router.get('/verified', getAllVerifiedUsers);
+router.get('/activities/:userId', getActivities);
+router.get('/stats/:userId', getUserStats);
+router.get('/college/:collegeName/activity', getCollegeActivity);
+router.patch('/activity/:activityId/verify', verifyActivity);
 export default router;
 //# sourceMappingURL=userRoutes.js.map
