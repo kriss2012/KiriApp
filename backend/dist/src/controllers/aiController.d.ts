eb2fe00c4d92476279cbcc8bafe810a58fa771cd
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-19 11:11:03 +0530

    Update aiController.d.ts

--- backend/dist/src/controllers/aiController.d.ts
+++ backend/dist/src/controllers/aiController.d.ts
@@ -1,4 +1,4 @@
 import type { Request, Response } from 'express';
-export declare const getAiHistory: (req: Request, res: Response) => Promise<void>;
+export declare const getAiHistory: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
 export declare const chatWithKiri: (req: Request, res: Response) => Promise<void>;
 //# sourceMappingURL=aiController.d.ts.map
