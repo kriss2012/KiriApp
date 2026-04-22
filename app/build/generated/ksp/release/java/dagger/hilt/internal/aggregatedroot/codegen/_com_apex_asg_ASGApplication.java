aea118d663e75400fba3eb6450bcea71b76e7e52
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-22 17:35:12 +0530

    Create _com_apex_asg_ASGApplication.java

---
+++ app/build/generated/ksp/release/java/dagger/hilt/internal/aggregatedroot/codegen/_com_apex_asg_ASGApplication.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.apex.asg.ASGApplication",
+    rootPackage = "com.apex.asg",
+    originatingRoot = "com.apex.asg.ASGApplication",
+    originatingRootPackage = "com.apex.asg",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "ASGApplication",
+    originatingRootSimpleNames = "ASGApplication"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_apex_asg_ASGApplication {
+}
