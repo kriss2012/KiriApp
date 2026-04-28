c6cbcb0d27278512e1b85dcefaa67ee4bd641ffc
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-28 13:39:51 +0530

    Update userRoutes.ts

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -3,11 +3,9 @@ import {
     getAllVerifiedUsers, 
     getProfile, 
     searchUsers, 
-    toggleEventAccess, 
     updateProfile,
     getActivities,
     getUserStats,
-    getCollegeActivity,
     verifyActivity
 } from '../controllers/userController.js';
 
@@ -18,11 +16,9 @@ const router = Router();
 router.get('/', searchUsers);
 router.get('/profile/:userId', authenticate, getProfile);
 router.put('/profile/:userId', authenticate, updateProfile);
-router.put('/toggle-access/:userId', toggleEventAccess);
 router.get('/verified', getAllVerifiedUsers);
 router.get('/activities/:userId', getActivities);
 router.get('/stats/:userId', getUserStats);
-router.get('/college/:collegeName/activity', getCollegeActivity);
 router.patch('/activity/:activityId/verify', verifyActivity);
 
 export default router;
