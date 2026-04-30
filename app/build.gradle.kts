52ad19c7fa793e1b7a0567aaf323b621693edd81
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-30 20:08:47 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,8 +76,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    ksp("com.google.dagger:hilt-compiler:2.51.1")
+    implementation("com.google.dagger:hilt-android:2.55")
+    ksp("com.google.dagger:hilt-compiler:2.55")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     
     // Retrofit & Networking
