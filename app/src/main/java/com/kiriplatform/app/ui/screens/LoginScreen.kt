ea771ba718aa610ae3bf73e6f660b5aca3dc5c65
Krishna Patil <krishnacpatil2006@gmail.com>
2026-05-01 14:16:11 +0530

    Update LoginScreen.kt

--- app/src/main/java/com/kiriplatform/app/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/kiriplatform/app/ui/screens/LoginScreen.kt
@@ -2,7 +2,6 @@ package com.kiriplatform.app.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
