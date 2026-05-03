002500352ffca937bf85a568dac2ec831cbf7fff
Krishna Patil <krishnacpatil2006@gmail.com>
2026-05-03 19:35:15 +0530

    Update RegisterScreen.kt

--- app/src/main/java/com/kiriplatform/app/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/kiriplatform/app/ui/screens/RegisterScreen.kt
@@ -2,6 +2,7 @@ package com.kiriplatform.app.ui.screens
 
 import androidx.compose.animation.*
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
