0aa8e39b11c3b0d4f1e07a9467b13eba0f1dbd41
Krishna Patil <krishnacpatil2006@gmail.com>
2026-05-14 17:32:11 +0530

    Update UiUtils.kt

--- app/src/main/java/com/kiriplatform/app/utils/UiUtils.kt
+++ app/src/main/java/com/kiriplatform/app/utils/UiUtils.kt
@@ -16,8 +16,6 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import io.github.fletchmckee.liquid.liquid
-import io.github.fletchmckee.liquid.rememberLiquidState
 
 fun Modifier.glassmorphism(
     enabled: Boolean = true,
