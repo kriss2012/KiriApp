b9725804e6a0eb4d9d64fbf5666e2b2ab77b1c81
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-06-05 17:14:15 +0530

    d

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -55,6 +55,9 @@ app.use('/api/board', boardRoutes);
 app.get('/', (req, res) => {
     res.send('ASG Community API is running...');
 });
+app.get('/api', (req, res) => {
+    res.send('ASG Community API is running...');
+});
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
